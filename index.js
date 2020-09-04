70eb305672566d81ddc0f40619fcb589d8111a40
Nic Galluzzo <nicolas@knack.com>
2020-09-04 16:57:35 -0700

    send pushbullet notification. check every 1 minute

--- index.js
+++ index.js
@@ -35,10 +35,30 @@ async function isGpuForSale(product) {
   return true
 }
 
-async function sendPushBulletNotification(product) {
+async function sendPushBulletNotification(device,product) {
 
   const noteUrl = `https://www.nvidia.com/en-us/geforce/graphics-cards/30-series/rtx-3080/`
   const note = `${product} is for sale!`
-  
-  pusher.link(device_id, product, noteUrl, note)
+
+  pusher.link(device, product, noteUrl, note)
 }
+
+async function inStockNotification(product) {
+
+  const stockStatus = await isGpuForSale(product)
+
+  if (!stockStatus) {
+
+    console.log(`GPU is not in stock :(`)
+    return
+  }
+
+  console.log(`GPU is in stock! Sending notification`)
+
+  sendPushBulletNotification(device_id,product)
+}
+
+setInterval(function() {
+
+  inStockNotification(`RTX 3080`)
+}, 60000)
