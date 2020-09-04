dcf485e3cd7f0c9bf7ebdd7eff86130b85dcfcba
Nic Galluzzo <nicolas@knack.com>
2020-09-04 16:33:44 -0700

    implement pushbullet notification

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const axios = require('axios')
+const PushBullet = require('pushbullet')
+
+const pusher = new PushBullet(process.env.PUSHBULLET_API_KEY)
+const device_id = process.env.DEVICE_ID
 
 async function searchNvidiaProduct(product) {
   
@@ -23,10 +27,15 @@ async function isGpuForSale(product) {
 
   const GpuDetails = GpuSearch.searchedProducts.productDetails[0]
 
-  if (GpuDetails.prdStatus !== `gf_notify_me`) {
+  if (GpuDetails.prdStatus !== `buy_now`) {
 
     return false
   }
 
   return true
+}
+
+async function sendPushBulletNotification(product) {
+
+  pusher.link(`ujyjz1ykpI4sjAuXDo0g56`, product, `https://www.nvidia.com/en-us/geforce/graphics-cards/30-series/rtx-3080/`, `${product} is for sale!`)
 }
